bab7b39a4deeead8ff4483b4156fccf654a55676
DS Rathore <dsrathorebsw1234@gmail.com>
2021-08-21 16:57:27 +0530

    Element

---
+++ 9.Elements.js
@@ -0,0 +1,23 @@
+// var para = document.getElementsByClassName("para")[0].innerHTML;
+// console.log(para);
+
+// var title = document.querySelector('.heading');
+// console.log(title);
+
+var counter = document.querySelector(".num");
+var follower = document.querySelector("#follower");
+
+let count = 0;
+
+setInterval(() => {
+    if (count < 1000) {
+        count++;
+        counter.innerText = count;
+    }
+}, 1);
+
+setTimeout(() => {
+    if(count < 1000){
+        follower.innerText = "Your Followers are " + count;  
+    }
+}, 5000);
