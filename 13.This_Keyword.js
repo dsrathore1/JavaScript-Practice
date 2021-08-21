6ffd4092a1351b1537ec1d0883974cf18b417de7
DS Rathore <dsrathorebsw1234@gmail.com>
2021-08-21 17:13:21 +0530

    This Keyword

--- 13.This_Keyword.js
+++ 13.This_Keyword.js
@@ -6,9 +6,9 @@ console.log(this);
 
 //* Regular Function 
 
-function sum(){
+function sum() {
     var add = 2 + 3;
-    console.log("This is an sum of two no. "+add);
+    console.log("This is an sum of two no. " + add);
     console.log(this); //? This showing "Undefined" in terminal but on web it will 1show the windows object. Windows object is a global object.
 }
 
@@ -27,5 +27,26 @@ const king = {
 }
 king.sum();
 
-//* In function
+//* In function, when we use "use strict" then it will show the output undefined.
 
+
+//! EXERCISE
+
+const exercise = {
+    name: "King",
+    branch: "CS",
+    func: function () {
+        let pro_lang = "JavaScript";
+        console.log("What is your fav. language", pro_lang);
+        console.log(this)
+
+        function multi(z, y) {
+            let mul = y * z;
+            console.log("The multiple value of two no. is",mul);
+            console.log(this) //! This show "undefined" rather it show's owner object because in regular function it will show only global object.
+        }   
+        multi(12, 53);
+    }
+}
+
+exercise.func();
