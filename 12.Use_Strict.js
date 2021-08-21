42f9c38a98d7ac467cfc21ec44c1f4c08a4ecfc0
DS Rathore <dsrathorebsw1234@gmail.com>
2021-08-21 16:58:21 +0530

    Use Strict

--- 12.Use_Strict.js
+++ 12.Use_Strict.js
@@ -2,22 +2,22 @@
 
 //! This is Hoisting function and this is not a good practice to use. When we write "use strict" then we have to use var, let, const, when we declare the variable this is good practice to avoid errors and make code cleaner. 
 
-// a(55,20);
+a(55,20);
 
-// function a (a, b){
-//     multi = a * b;
-//     console.log(add);
-// }
+function a (a, b){
+    multi = a * b;
+    console.log(add);
+}
 
 //? In this function we do every thing right but we declare two parameters (Duplicate parameter) and that is not allow into this context
 
 
-// function b(x, x) {
-//     var add = x + x;
-//     console.log(add);
-// }
+function b(x, x) {
+    var add = x + x;
+    console.log(add);
+}
 
-// b(12, 32);
+b(12, 32);
 
 
 //* Correct one
